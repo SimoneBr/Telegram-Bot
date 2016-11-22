2497039d03bd163779cfa0a34bfb4830de9325fc
SimoneBr <braccisimone.11@gmail.com>
2016-11-22 16:47:05 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,11 +19,12 @@ $text = isset($message['text']) ? $message['text'] : "";
 //$text = trim($text);
 //$text = strtolower($text);
 $text = "";
-if (strcmp ( $text , "/trump" )==0){
+if ( $text === "/trump" ){
   $text = "http://www.lastampa.it/rf/image_lowres/Pub/p4/2016/11/21/Esteri/Foto/RitagliWeb/TrumpHealthCareLawJPEG-701cf_1479755911-kLIC-U1090184008278543C-1024x576@LaStampa.it.jpg";
 }
 header("Content-Type: application/json");
 $parameters = array('chat_id' => $chatId, "photo" => $text);
 $parameters["method"] = "sendPhoto";
-echo json_encode($parameters);
 echo $text;
+echo json_encode($parameters);
+
