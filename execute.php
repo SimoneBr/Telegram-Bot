b3a8a82ec11819dc4270742b16b3313cd3f0c682
SimoneBr <braccisimone.11@gmail.com>
2016-11-22 16:25:30 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -19,8 +19,8 @@ $text = isset($message['text']) ? $message['text'] : "";
 //$text = trim($text);
 //$text = strtolower($text);
 
-$text = "vai a fare in culo";
+$text = "http://www.lastampa.it/rf/image_lowres/Pub/p4/2016/11/21/Esteri/Foto/RitagliWeb/TrumpHealthCareLawJPEG-701cf_1479755911-kLIC-U1090184008278543C-1024x576@LaStampa.it.jpg";
 header("Content-Type: application/json");
-$parameters = array('chat_id' => $chatId, "text" => $text);
-$parameters["method"] = "sendMessage";
+$parameters = array('chat_id' => $chatId, "photo" => $text);
+$parameters["method"] = "sendPhoto";
 echo json_encode($parameters);
