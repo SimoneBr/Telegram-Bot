57ac68c6dfd92f2f46a87050f20cdf144eeeb4f0
SimoneBr <braccisimone.11@gmail.com>
2016-11-22 15:12:09 +0100

    Update register.php

--- register.php
+++ register.php
@@ -1,8 +1,8 @@
 <?php
 
 // PARAMETRI DA MODIFICARE
-$WEBHOOK_URL = 'https://{APP NAME}.herokuapp.com/execute.php';
-$BOT_TOKEN = '{TOKEN}';
+$WEBHOOK_URL = 'https://lit-woodland-16216.herokuapp.com/execute.php';
+$BOT_TOKEN = '182266714:AAHt1sPaNoMocKDhFE8kvqAJjyrX9LYFGkc';
 
 // NON APPORTARE MODIFICHE NEL CODICE SEGUENTE
 $API_URL = 'https://api.telegram.org/bot' . $BOT_TOKEN .'/';
